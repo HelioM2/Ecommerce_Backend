82f790e90c14307e00a034b9cd08071217a7e4a2
HelioM2 <hantunes059@gmail.com>
2025-05-12 17:27:55 +0100

    configurar db.js para conectar com railway

--- config/db.js
+++ config/db.js
@@ -1,12 +1,27 @@
+// const mysql = require('mysql2/promise');
+
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'ecommerce',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+// });
+
+// module.exports = pool;
+
 const mysql = require('mysql2/promise');
 
+// Use variáveis de ambiente para segurança e flexibilidade
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'ecommerce',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'ecommerce',
   waitForConnections: true,
   connectionLimit: 10,
 });
 
 module.exports = pool;
+
